80fffc0d61d391c21752caf476d5c307a11ee7c6
Calum McBride <calum.mcbride@gmail.com>
2022-10-27 11:49:45 -0400

    added alternating list color

--- storageServer.lua
+++ storageServer.lua
@@ -7,10 +7,27 @@ local chest = peripheral.wrap("right")
 local items = chest.list()
 
 local itemsWindow = window.create(term.current(), 1, 3, 51, 15, false)
-itemsWindow.setBackgroundColour(colours.red)
 itemsWindow.setTextColour(colours.white)
 itemsWindow.clear()
-itemsWindow.write("Testing my window!")
+
+-- build table with meta data
+for k, v in pairs(items) do
+    items[k] = chest.getItemMeta(k)
+end
+
+local isGrey = true
+for k, v in pairs(items) do
+    if isGrey then
+        itemsWindow.setBackgroundColour(colours.lightGrey)
+        isGrey = false
+    else
+        itemsWindow.setBackgroundColour(colours.black)
+        isGrey = true
+    end
+    
+    itemsWindow.write(items[k].displayName .. " " .. items[k].count)
+end
+
 itemsWindow.setVisible(true)
 
 os.sleep(4)
